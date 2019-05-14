ec211f09b3129b2de0121dfa8b79cdea28060c77
Cbain10 <50216002+Cbain10@users.noreply.github.com>
2019-05-13 18:23:13 -0600

    https commit - DONE

--- script.js
+++ script.js
@@ -6,10 +6,8 @@ window.onload = function() {
             return;
         console.log(value);
 
-        const url = "http://api.openweathermap.org/data/2.5/weather?q=" + value + ",US&units=imperial"
+        const url = "https://api.openweathermap.org/data/2.5/weather?q=" + value + ",US&units=imperial"
             + "&APPID=d4e3c4a280191749cda2ecce91b9750d";
-        // const newUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + value
-        //     + "&APPID=d4e3c4a280191749cda2ecce91b9750d";
 
         try {
             // trying to do something that might fail
@@ -19,7 +17,7 @@ window.onload = function() {
             let results = "";
             results += '<div class="daily-forecast"><h2 id="weather-title">Current weather in ' + json.name + "</h2>";
             for (let i=0; i < json.weather.length; i++) {
-                results += '<img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
+                results += '<img src="https://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
             }
             results += '<h2>' + json.main.temp + " &deg;F</h2>"
             results += "<p>"
@@ -38,7 +36,7 @@ window.onload = function() {
         } catch(err){
           console.log(err);
         }
-        const url2 = "http://api.openweathermap.org/data/2.5/forecast?q=" + value + ", US&units=imperial"
+        const url2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + value + ", US&units=imperial"
             + "&APPID=d4e3c4a280191749cda2ecce91b9750d";
         fetch(url2)
             .then(function(response) {
@@ -48,7 +46,7 @@ window.onload = function() {
             for (let i=0; i < 6; i++) {
                 forecast1 += "<h2>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY, h:mm:ss a') + "</h2>";
                 forecast1 += "<p>Temperature: " + json.list[i].main.temp + "</p>";
-                forecast1 += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/></div>'
+                forecast1 += '<img src="https://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/></div>'
                 forecast1 += "<p>High: " + json.list[i].main.temp_max + "</p>";
                 forecast1 += "<p>Low: " + json.list[i].main.temp_min + "</p>";
                 forecast1 += "<p>Humidity: " + json.list[i].main.humidity + "</p>";
@@ -58,7 +56,7 @@ window.onload = function() {
             for (let i=6; i < 14; i++) {
                 forecast2 += "<h2>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY, h:mm:ss a') + "</h2>";
                 forecast2 += "<p>Temperature: " + json.list[i].main.temp + "</p>";
-                forecast2 += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/></div>'
+                forecast2 += '<img src="https://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/></div>'
                 forecast2 += "<p>High: " + json.list[i].main.temp_max + "</p>";
                 forecast2 += "<p>Low: " + json.list[i].main.temp_min + "</p>";
                 forecast2 += "<p>Humidity: " + json.list[i].main.humidity + "</p>";
@@ -68,7 +66,7 @@ window.onload = function() {
             for (let i=14; i < 22; i++) {
                 forecast3 += "<h2>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY, h:mm:ss a') + "</h2>";
                 forecast3 += "<p>Temperature: " + json.list[i].main.temp + "</p>";
-                forecast3 += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/></div>'
+                forecast3 += '<img src="https://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/></div>'
                 forecast3 += "<p>High: " + json.list[i].main.temp_max + "</p>";
                 forecast3 += "<p>Low: " + json.list[i].main.temp_min + "</p>";
                 forecast3 += "<p>Humidity: " + json.list[i].main.humidity + "</p>";
@@ -78,7 +76,7 @@ window.onload = function() {
             for (let i=22; i < 30; i++) {
                 forecast4 += "<h2>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY, h:mm:ss a') + "</h2>";
                 forecast4 += "<p>Temperature: " + json.list[i].main.temp + "</p>";
-                forecast4 += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/></div>'
+                forecast4 += '<img src="https://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/></div>'
                 forecast4 += "<p>High: " + json.list[i].main.temp_max + "</p>";
                 forecast4 += "<p>Low: " + json.list[i].main.temp_min + "</p>";
                 forecast4 += "<p>Humidity: " + json.list[i].main.humidity + "</p>";
@@ -88,7 +86,7 @@ window.onload = function() {
             for (let i=30; i < 38; i++) {
                 forecast5 += "<h2>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY, h:mm:ss a') + "</h2>";
                 forecast5 += "<p>Temperature: " + json.list[i].main.temp + "</p>";
-                forecast5 += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/></div>'
+                forecast5 += '<img src="https://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/></div>'
                 forecast5 += "<p>High: " + json.list[i].main.temp_max + "</p>";
                 forecast5 += "<p>Low: " + json.list[i].main.temp_min + "</p>";
                 forecast5 += "<p>Humidity: " + json.list[i].main.humidity + "</p>";
@@ -98,7 +96,7 @@ window.onload = function() {
             for (let i=38; i < 40; i++) {
                 forecast6 += "<h2>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY, h:mm:ss a') + "</h2>";
                 forecast6 += "<p>Temperature: " + json.list[i].main.temp + "</p>";
-                forecast6 += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/></div>'
+                forecast6 += '<img src="https://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/></div>'
                 forecast6 += "<p>High: " + json.list[i].main.temp_max + "</p>";
                 forecast6 += "<p>Low: " + json.list[i].main.temp_min + "</p>";
                 forecast6 += "<p>Humidity: " + json.list[i].main.humidity + "</p>";
